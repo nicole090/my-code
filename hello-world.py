9c87e2ab3d5ca582e9c7437d19adeef420be7ea6
童 <tong@tongdeMacBook-Pro.local>
2019-09-07 02:15:34 +0800

    配置cpp

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,9 @@
-print('helloword')
-
-
-
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+ 
+fruits = ['banana', 'apple',  'mango']
+for fruit in fruits:           
+    print('当前水果 :', fruit) 
+ 
+print ("Good bye!")
